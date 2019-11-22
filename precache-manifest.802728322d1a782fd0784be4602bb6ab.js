4fdbc43b431a378c47d473de0a97702e3b8ddf67
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-22 13:31:16 +0000

    Updates

---
+++ precache-manifest.802728322d1a782fd0784be4602bb6ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07261d78f7914a330919ee48ac97638d",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "b0bcdf4d7d8c249b5b0a",
+    "url": "/address-book/static/css/main.10346d24.chunk.css"
+  },
+  {
+    "revision": "87e157f3ff653c2b2049",
+    "url": "/address-book/static/js/2.46476931.chunk.js"
+  },
+  {
+    "revision": "b0bcdf4d7d8c249b5b0a",
+    "url": "/address-book/static/js/main.453c9c63.chunk.js"
+  },
+  {
+    "revision": "d187e99c48a13f20c898",
+    "url": "/address-book/static/js/runtime-main.60ba8be0.js"
+  }
+]);
